f6361ead59ed35ad2672dfe42ea5968bc4e64f32
abi <algimenez@fi.uba.ar>
2024-12-07 15:24:16 -0300

    subo cambios de Miri y Ro

--- pinterest.js
+++ pinterest.js
@@ -1,13 +1,17 @@
 const images = [
-  "https://kanykathotel.com/wp-content/uploads/2020/09/porque-elegir-una-residencia-de-lujo-perros-y-gatos.webp",
-  "https://plus.unsplash.com/premium_photo-1732569119693-05321f406646?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/200x300",
-  "https://images.unsplash.com/photo-1733031510035-3fda1ac87b8d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/300x400",
-  "https://images.unsplash.com/photo-1733028724656-b456573528ee?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/200x300",
-  "https://images.unsplash.com/photo-1732888169391-9001089d6342?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/400x300",
-  "https://images.unsplash.com/photo-1732967377738-66c8dfb45167?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/300x300"
+  "https://plus.unsplash.com/premium_photo-1682285866576-7d2b8490cb10?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://i.pinimg.com/736x/66/3c/fc/663cfc3b279c8d1023157b5b68be1107.jpg",
+  "https://ideasconsejos.com/images/2020/11/Fotos-para-tomarte-con-tu-mejor-amiga-inmediatamente-12.jpg",
+  "https://images.unsplash.com/photo-1632616428087-bc0aeb7af242?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://4.bp.blogspot.com/-WYHBetuHAR4/VnTVSv5c8yI/AAAAAAAAa7w/FBBgqYuK9CM/s1600/Luis%2BAbinader.jpg",
+  "https://cdn.pixabay.com/photo/2024/05/14/20/52/selfie-8762129_1280.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQklsj54WowaVK0SKQzWitRWEOtWUye9vnuuA&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNKlOFK1j_LeSiZHGnLr8Rfi2xXZRh58cotA&s",
+  "https://plus.unsplash.com/premium_photo-1683129651802-1c7ba429a137?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zmllc3RhJTIwZGp8ZW58MHx8MHx8fDA%3D",
 ];
 
 const grid = document.getElementById("imageGrid");
+
 
 images.forEach((imageSrc) => {
   const gridItem = document.createElement("div");
@@ -23,7 +27,7 @@ images.forEach((imageSrc) => {
   img.setAttribute('class', 'gallary_img')
   img.style.width = "100%";
 
-  likeIcon.setAttribute('class', 'material-symbols-outlined active like_img')
+  likeIcon.setAttribute('class', 'material-symbols-outlined actives like_img')
   likeIcon.textContent ="favorite"
 
   like.setAttribute('class', 'uil uil-heart-alt like_img_animation')
@@ -39,7 +43,7 @@ images.forEach((imageSrc) => {
 
     img.addEventListener('mouseup', (evt) => {
       if(isLike){
-        likeIcon.setAttribute('class','material-symbols-outlined active like_img')
+        likeIcon.setAttribute('class','material-symbols-outlined actives like_img')
         
       } else {
         likeIcon.setAttribute('class', 'material-symbols-outlined like_img')
@@ -56,6 +60,5 @@ images.forEach((imageSrc) => {
     });
 
 });
-
 
 
